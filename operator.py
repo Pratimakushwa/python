f3480500e4dd91c70898af9ab6fe57cf2fb1f18f
Pratima kushwa <pratimakushwaha479@gmail.com>
2025-04-25 12:22:33 +0530

    operator

---
+++ operator.py
@@ -0,0 +1,20 @@
+a=10
+b=20
+print (a and b)
+a=0
+b=20
+print (a and b)
+a=10
+b=0
+print (a and b)
+a=10
+b=20
+print (a or b)
+a=0
+b=20
+print (a or b)
+a=10
+b=0
+print (a or b)
+li =["apple","orange","coconut"]
+print("app" in li)
--- rectangle.py
+++ rectangle.py
@@ -1,3 +1,4 @@
 b = int (input("enter the base"))
 h = int (input("enter the height"))
 print (b*h)
+a =10
