042d1c9b284de27936e90e00d92dbebacae538a8
Pratima kushwa <pratimakushwaha479@gmail.com>
2025-04-25 12:32:58 +0530

    list

---
+++ .ipynb_checkpoints/insert.py
@@ -0,0 +1,9 @@
+li=[1,3,4,5,6]
+li.insert(3,100)
+print(len(li))
+li.append(2)
+print(li)
+li.pop()
+print(li)
+li.sort()
+print(li)
--- rectangle.py
+++ rectangle.py
@@ -1,4 +1,5 @@
-b = int (input("enter the base"))
-h = int (input("enter the height"))
-print (b*h)
-a =10
+# b = int (input("enter the base"))
+# h = int (input("enter the height"))
+# print (b*h)
+li=[1,2,3,4,5,6,7,8,9]
+li.insert(3,100)
