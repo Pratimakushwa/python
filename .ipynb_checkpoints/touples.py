61a4c5f4f317df214dbb41d03852814b315acc09
Pratima kushwa <pratimakushwaha479@gmail.com>
2025-04-29 22:00:35 +0530

    touple

---
+++ .ipynb_checkpoints/touples.py
@@ -0,0 +1,13 @@
+touple=("apple","bnana", "orange")
+apple= "mango"
+print(touple)
+print(touple[0])
+touple[:: -1]
+print(touple)
+list=[11,23,4,5,5]
+
+print (list[0])
+print(list[-1])
+print (len(list))
+print (list.remove(23))
+print (list[max])
