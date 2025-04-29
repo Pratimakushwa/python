971c44d675935398803e4078b1360c30ea05dfb4
Pratima kushwa <pratimakushwaha479@gmail.com>
2025-04-29 22:27:51 +0530

    operator

--- .ipynb_checkpoints/touples.py
+++ .ipynb_checkpoints/touples.py
@@ -1,13 +1,24 @@
-touple=("apple","bnana", "orange")
-apple= "mango"
-print(touple)
-print(touple[0])
-touple[:: -1]
-print(touple)
-list=[11,23,4,5,5]
+# # touple=("apple","bnana", "orange")
+# # apple= "mango"
+# # print(touple)
+# # print(touple[0])
+# # touple[:: -1]
+# # print(touple)
 
-print (list[0])
-print(list[-1])
-print (len(list))
-print (list.remove(23))
-print (list[max])
+# list=[11,23,4,5,5]
+
+# print (list[0])
+# print(list[-1])
+# print (len(list))
+# print (list.remove(23))
+# print(list)
+
+a = 20
+b=2
+print( a>b)
+print(a==b)
+print(a!=b)
+print(a%b)
+print(a**b)
+print(a/b)
+print(a)
