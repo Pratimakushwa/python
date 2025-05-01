d82eebf7d1a09448ac1208325fc2e8bf468148a9
Pratima kushwa <pratimakushwaha479@gmail.com>
2025-05-01 12:36:29 +0530

    dictionary

--- .ipynb_checkpoints/touples.py
+++ .ipynb_checkpoints/touples.py
@@ -31,16 +31,52 @@
 # print(fact) 
 
 
-tup=(1,3,4,5,6,7)
-min =tup[0]
-for i in tup:
-    if i<min:
-        min = i
-print(min)
+# tup=(1,3,4,5,6,7)
+# min =tup[0]
+# for i in tup:
+#     if i<min:
+#         min = i
+# print(min)
 
-set={1,3,4,5,5}
-a=set 
-a = set.copy()
-set.add(20)
-print(a)
-print(set)
+# set={1,3,4,5,5}
+# a=set 
+# a = set.copy()
+# set.add(20)
+# print(a)
+# print(set)
+# set={1,3,4,5,6,7}
+# set.add()
+# # set.clear()
+# # set.pop()
+
+# print(set1)
+
+
+# dic={ 100: "durga",200:"ravi",300:"shiva"}
+# print(dic)
+# dic[400]= "pavan"
+# print(dic)
+# dic[100]="sunny"
+# print(dic)
+# # print(dic.keys())
+# # print(dic.values())
+# # print(dic.items())
+# # print(dic.clear())
+# # dic.clear()
+# # print(dic)
+# # del dic[100]
+# # print(dic)
+
+
+# # print(dic)
+# # dic.clear()
+# # print(dic)
+# dic.get(200)
+# print(dic)
+
+d={"name":"manoj","age":24}
+# d.pop("name")
+# d.popitem()
+print(d)
+d.setdefault("grade","A")
+print(d)
