e978fc7fba9592dd01eae0f03702fd40b3d5c46b
Pratima kushwa <pratimakushwaha479@gmail.com>
2025-05-05 20:46:00 +0530

    ifelse

--- .ipynb_checkpoints/slice.py
+++ .ipynb_checkpoints/slice.py
@@ -1,21 +1,38 @@
-# s="hello"
+# # s="hello"
+# # # print(s[-1:-5:-1])
+# # # print(s[-1:-6:-1])
+# # # print(s[0:0])
+# # # print(s[1:5:1])
 # # print(s[-1:-5:-1])
-# # print(s[-1:-6:-1])
-# # print(s[0:0])
-# # print(s[1:5:1])
-# print(s[-1:-5:-1])
-# print(s[1:5:2])
-# print(s[-1:-5:-2])
+# # print(s[1:5:2])
+# # print(s[-1:-5:-2])
 
 # a = eval(input("enter the number"))
 # print(a)
-# a = input("enter the number")
-# print(a)
-a=20
-b=30
-if a>b:
-    print(a)
+# # a = input("enter the number")
+# # print(a)
+# a=20
+# b=30
+# if a>b:
+#     print(a)
+# else:
+#     print(b)
+ 
+# str1="pratima"
+# # -7-6-5-4-3-2-1
+# # pratima
+# # 0123456
+# # print(str1[1:5])
+# # print(str1[-1:5])
+# # print(str1[1:-5])
+# # print(str1[-1:-5:-1])
+# print(str1[1:-2:-1])
+# print(str1[-1:22:])
+# print(str1[-4:-7:-1])
+num = int(input("enter the number"))
+if num>0:
+    print("positive")
 else:
-    print(b)
- 
- 
+    print("negative")
+
+
