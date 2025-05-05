5556565c89732b779dfe6b7300f850709acb8374
Pratima kushwa <pratimakushwaha479@gmail.com>
2025-05-05 12:27:41 +0530

    slice

---
+++ .ipynb_checkpoints/operaters.py
@@ -0,0 +1,35 @@
+# arthematic operator
+# a= 20
+# b=30
+# # print(a+b)
+# # print(a-
+# # print(a*b)
+# # print(a/b)
+# # print(a//b)
+# # print(a**b)
+# # assignment operaton
+# print("true") if (a==b) else print("false")
+# print(a or b)
+# print(a in b)
+# datatype
+# list= [1,8,2,3,4,5,6,7,8,9]
+# # list[0]= "8"
+# # print(list)
+# # list.append(24)
+# # print(list)
+# # list.pop()
+# # print(list)
+# list.count(1)
+# print(list)
+# list.sort(reverse=True)
+# print(list)
+# list.reverse()
+# print(list)
+# list.extend(6,7,8,9,0)
+# print(list)
+
+tuple=(1,2,3,4,5,5,6,7,8)
+print(min(tuple))
+print(max(tuple))
+
+
---
+++ .ipynb_checkpoints/slice.py
@@ -0,0 +1,21 @@
+# s="hello"
+# # print(s[-1:-5:-1])
+# # print(s[-1:-6:-1])
+# # print(s[0:0])
+# # print(s[1:5:1])
+# print(s[-1:-5:-1])
+# print(s[1:5:2])
+# print(s[-1:-5:-2])
+
+# a = eval(input("enter the number"))
+# print(a)
+# a = input("enter the number")
+# print(a)
+a=20
+b=30
+if a>b:
+    print(a)
+else:
+    print(b)
+ 
+ 
