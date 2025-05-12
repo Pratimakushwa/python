4ac4bd7e32117e0b84bdd5b20cd395d229dba571
Pratima kushwa <pratimakushwaha479@gmail.com>
2025-05-12 19:14:15 +0530

    nesting

---
+++ .ipynb_checkpoints/electricity.py
@@ -0,0 +1,59 @@
+
+
+
+# # marks = int(input("enter the number"))
+# # if marks>=80 and marks<=100:
+# #     print("A")
+# # elif marks>60 and marks<=80:
+# #     print("B")
+# # elif marks>=30 and marks<=60:
+# #     print("C")
+# # elif marks>=0 and marks<=30:
+# #     print("D")
+
+# n1= int (input("enter the number1"))
+# n2= int(input("enter the number2"))
+# op= (input("enter the operator"))
+# if (op=='+'):
+#     print(n1+n2)
+# elif op == '/':
+#     print(n1/n2)
+# elif op == '*':
+#     print (n1*n2)
+# else:
+#     print("invalid") 
+
+# num=int(input("enter the is number"))
+# if num>0:
+#     print("positive")
+# elif num==0:
+#     print("zero")
+# elif num<0:
+#     print("negative")
+# else:
+#     print("invalid")
+
+# a= int(input("enter the number"))
+# b= int(input("enter the number"))
+# c= int(input("enter the number"))
+
+# if a>b and a>c:
+#     print(" a is greatest")
+# elif b>a and b>c:
+#     print("b is greatest")
+# elif c>a and  c>b:
+#     print("c is greatest")
+# else:
+#     print("invailed")
+# website= input("enter your website")
+# if website.lower() =="flipkart":
+    
+#     s = input("enter the 'product' or 'service':")
+#     if s=="product":
+#      p = input(" clothes or shoes")
+#     if p=="clothe":
+#        print("price:2000")
+#     elif p=="shoes":
+#        print("price:3000")
+#     else:
+#        print("invalid")
