4a9a8d316c6f805f49a17259107aa880b8ecd8c2
Pratima kushwa <pratimakushwaha479@gmail.com>
2025-05-17 15:22:28 +0530

    ass

--- swap.py
+++ swap.py
@@ -25,9 +25,88 @@
 #         mx=li[i]
 # print(mx)
 
-li=[1,2,3,4,56]
-min=56
-for i in range(len( li)):
-    if min >li[i]:
-        min=li[i]
-print(min)
+# li=[1,2,3,4,56]
+# min=56
+# for i in range(len( li)):
+#     if min >li[i]:
+#         min=li[i]
+# print(min)
+
+
+# li=[1,2,3,4,56]
+# min=9
+# for i in range(len( li)):
+#     if min >li[i]:
+#         min=li[i]
+# print(min)
+
+
+# num= int(  input("enter the number"))
+# if num%5==0 and num%3==0:
+#     print("true")
+# else:
+#   print("false")
+
+# num= int(input("enter the enumber"))
+# if num>0:
+#     print("positive")
+# else:
+#     print("negative")
+
+
+# num1= int(input("enter the first number"))
+# num2= int(input("enter the second  number"))
+# if num1>num2:
+#     print("num1 is greatest number")
+# elif num2>num1:
+#     print("num2 is greatest")
+# else:
+#     print("invalid")
+
+# num1 = int(input("enter the first number"))
+# num2= int(input("enter the second number"))
+# num3 = int(input("enter the third number"))
+# if num1>num2 and num1>num3:
+#     print("num1 is a greatest")
+# elif num2>num1 and num2> num3:
+#     print("num2 is a greatest number")
+# elif num3>num1 and num3>num2:
+#     print("num3 is greatest")
+# else:
+#     print("invailed")
+
+# char=input("enter the charcter")
+# if char=='a' or char=='e' or char =='i' or char=='o' or char=='u':
+#     print("vowel")
+# else:
+#     print("consonent")
+
+# char = input("enter the charcter")
+# if char>='a' and  char>='z' and char>= 'A' and char>='Z':
+#     print("alphabet")
+# else:
+#     print("not a alphabet")
+
+# year= int (input("enter the year"))
+# if year%400==0 and year%4==0:
+#     print("leap year")
+# else:
+#     print("not a leap year")
+
+
+char= (input("enter the number"))
+if char>= 'a' and char<='z' and char>='A' and char<='Z':
+    print("alphabet")
+elif char>='0' and char>='9':
+    print("digit")
+else:
+    print("special symbol")
+
+
+# char= input("enter the charcter")
+# if char>='a' and char<='z':
+#     print("lowerCase")
+# elif  char>='A' and char<='Z':
+#     print(" upperCase")
+# else:
+#     print("invalid")
