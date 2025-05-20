758c1235e3588f21a0a998e473542f4546f6faff
Pratima kushwa <pratimakushwaha479@gmail.com>
2025-05-20 21:26:31 +0530

    py

---
+++ loop.py
@@ -0,0 +1,86 @@
+# num= int(input("enter the number"))
+# for i in range(1, num):
+#     print(i)
+
+# num= int(input("enter the numebr"))
+# for i in range( 1, num):
+#     if i%2==0:
+#         print(i)
+
+# num= int(input("enter the numebr"))
+# for i in range( 1, num):
+#     if i%2!=0:
+#         print(i)
+
+# num= int(input("enter the numebr"))
+# sum=0
+# for i in range( 1, num):
+#     sum=sum+i
+#     print("total sum :", sum)
+    
+# num= int(input("enter the numebr"))
+# sum=0
+# while num>0:
+#     digit= num % 10
+#     sum+=digit
+#     num=num// 10
+
+# print("sum of digit:",sum)
+    
+
+
+# num= int(input("enter the numebr"))
+# rev=0
+# while num>0:
+#     digit=num%10
+#     rev= rev*10+ digit
+#     num=num//10
+# print(rev)
+
+# num= int(input("enter the numebr"))
+# greatest=0
+# while num>0:
+#     digit= num % 10
+#     if digit>greatest:
+#         greatest=digit
+
+#     num=num// 10
+
+# print(greatest)
+    
+
+
+# num= int(input("enter the numebr"))
+# min=num%10
+# num= num//10
+# while num>0:
+#     digit= num % 10
+#     if digit<min:
+#         min=digit
+
+#     num=num// 10
+
+# print(min)
+    
+# num= int(input("enter the numebr"))
+# min=0
+
+# while num>0:
+    
+#     if num<min:
+#         min=num
+
+
+
+# # print(min)
+# num=int(input("inter the number"))
+# temp=num
+# rev=0
+# while num>0:
+#  digit= num%10
+#  rev=rev*10+digit
+#  num=num//10
+#  if temp==rev:
+#   print("palindrom")
+#  else:
+#   print("not a palindrom")
