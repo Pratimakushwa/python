8d19034ded2009865369e1fe14b8cc49c91f47bc
Pratima kushwa <pratimakushwaha479@gmail.com>
2025-05-21 12:29:11 +0530

    py

---
+++ pattarn.py
@@ -0,0 +1,81 @@
+
+# for i in range(1, 6):
+#     for j in range( 1,6):
+#         if(i>=j):
+#            print("*", end="")
+#         else:
+#          print(end=" ")
+#     print()
+
+       
+# for i in range(1, 6):
+#     for j in range( 1,6):
+#         if(i<=j):
+#            print("*", end="")
+#         else:
+#          print(end=" ")
+#     print()
+       
+# rows=5 
+# for i in range(1, rows+1):
+#     for j in range( 1, rows+1):
+#         if(i<=6-j):
+#            print("*", end="")
+#         else:
+#          print(end=" ")
+#     print()
+       
+# rows=5 
+# for i in range(1, rows+1):
+#     for j in range( 1, rows+1):
+#         if(i>=6-j):
+#            print("*", end="")
+#         else:
+#          print(end=" ")
+#     print()
+       
+
+
+
+row = int(input("Enter Number of rows: "))
+a=65
+alpha=chr(a)
+for r in range(1,row+1):
+    for c in range(1,row+1):
+        if c<=r:
+            print(alpha,end=" ")
+            a=a+1
+            alpha=chr(a)
+        else:
+            print(" ",end=" ")
+    print()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
