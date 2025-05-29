630dee8d34e09fddb08ae16544ea25e3351c7d65
Pratima kushwa <pratimakushwaha479@gmail.com>
2025-05-29 12:28:42 +0530

    py

--- practice.py
+++ practice.py
@@ -1,3 +1,30 @@
+#lambda funcntion (anonymus function)
+# lambda argument: expression
+# 1- Use for small solutions/functionality we use lambda function 
+# 2- To perform small task lambda function is more readable and faster then normal function
+# 3- Mostly use with high order functions like map() filter() and reduce()
+
+prime = lambda x: x>1 and all(x%i!=0 for i in range(2,int(x**0.5)+1))
+print(prime(5))
+ 
+
+greater = lambda x,y: x if x>y else y
+print(greater(15,8))
+
+# cube = lambda x: x*x*x
+# print(cube(6))
+
+# square = lambda x: x*x
+
+# print(square(5))
+
+# def square(x):
+#     return x*x
+
+# print(square(5))
+
+
+
 
 # num= int (input("enter the number"))
 # if num%2==0:
@@ -5,8 +32,8 @@
 # else:
 #     print("odd number")
 
-num= int(input("enter the number"))
-if num>0:
-    print("positive")
-else:
-    print("negative")
+# num= int(input("enter the number"))
+# if num>0:
+#     print("positive")
+# else:
+#     print("negative")
