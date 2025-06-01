4d062a97845bcd2d477a3d9901a6b9354e988d59
Pratima kushwa <pratimakushwaha479@gmail.com>
2025-06-01 16:31:23 +0530

    py

--- pattarn.py
+++ pattarn.py
@@ -75,7 +75,14 @@ for i in range(1, rows+1):
 
 
 
+# for i in range(1, 6):
+#     for j in range( 1,6):
+#         if(i>=j):
+#            print(j, end="")
 
+         
+#         print(end=" ")
+#     print()
 
 
 
