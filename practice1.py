f79c76aa68596e0c4ed50a318f5780295d07d582
Pratima kushwa <pratimakushwaha479@gmail.com>
2025-06-02 21:10:18 +0530

    py

--- practice1.py
+++ practice1.py
@@ -1,9 +1,12 @@
-num= int(input("enter the number"))
-if num>0:
-    print ("even")
-elif num==0:
-    print("zero")
-elif num==1:
-    print("one")
-else:
-    print("invalid")
+# num= int(input("enter the number"))
+# if num>0:
+#     print ("even")
+# elif num==0:
+#     print("zero")
+# elif num==1:
+#     print("one")
+# else:
+#     print("invalid")
+num= int (input("enter the number "))
+for i in range (1, num):
+    print(i)
