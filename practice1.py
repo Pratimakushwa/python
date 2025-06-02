81c8dacc45302ca3223fb5ee953dd035ab1d200c
Pratima kushwa <pratimakushwaha479@gmail.com>
2025-06-02 21:19:02 +0530

    py

--- practice1.py
+++ practice1.py
@@ -16,4 +16,8 @@ num= int (input("enter the number"))
 for i in range( num):
     # sum= sum+i
     print(i)
-
+year= int (input("enter the year"))
+if num%4==0 and num%400==0:
+    print("leap year")
+else:
+    print("not a leap year")
