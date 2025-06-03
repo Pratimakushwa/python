cf47841abaa1d274a49c98e80adec29e7f619ffa
Pratima kushwa <pratimakushwaha479@gmail.com>
2025-06-03 20:17:22 +0530

    py

--- practice1.py
+++ practice1.py
@@ -44,3 +44,27 @@ if num%4==0 and num%400==0:
     print("leap year")
 else:
     print("not a leap year")
+
+# num= int(input("enter the number"))
+# if num>0:
+#     print ("even")
+# elif num==0:
+#     print("zero")
+# elif num==1:
+#     print("one")
+# else:
+#     print("invalid")
+# num= int (input("enter the number "))
+# for i in range (1, num):
+#     print(i)
+
+num= int (input("enter the number"))
+# sum=0
+for i in range( num):
+    # sum= sum+i
+    print(i)
+year= int (input("enter the year"))
+if num%4==0 and num%400==0:
+    print("leap year")
+else:
+    print("not a leap year")
