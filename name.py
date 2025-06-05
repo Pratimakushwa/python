f13d25d7cd069cb3a98992ad5f2ef135aac787df
Pratima kushwa <pratimakushwaha479@gmail.com>
2025-06-05 20:36:34 +0530

    py

--- name.py
+++ name.py
@@ -1,5 +1,9 @@
-num= int (input("enter the number"))
-if num>0:
-    print ("positive")
-else:
-    print("negative")
+# num= int (input("enter the number"))
+# if num>0:
+#     print ("positive")
+# else:
+#     print("negative")
+num=int (input("enter the number"))
+for i in  range(1, num):
+    print(i)
+
