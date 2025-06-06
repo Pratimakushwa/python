37e5462fe77b7e2c451f02c893e19141c317af5e
Pratima kushwa <pratimakushwaha479@gmail.com>
2025-06-06 15:56:43 +0530

    py

--- name.py
+++ name.py
@@ -3,7 +3,19 @@
 #     print ("positive")
 # else:
 #     print("negative")
-num=int (input("enter the number"))
-for i in  range(1, num):
-    print(i)
+# num=int (input("enter the number"))
+# for i in  range(1, num):
+#     print(i)
+
+# num= int (input("enter the number"))
+# sum=0
+# for i in range(1,num+1):
+#     sum+=i
+# print(sum)
+num = int (input("enter the number"))
+for i in range(1,num):
+    if num%i==0:
+        print("square")
+    else:
+        print("not divisiable")
 
