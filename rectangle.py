13448fdf3e2a59c58a8f05a3f5dac1927afc41a2
Pratima kushwa <pratimakushwaha479@gmail.com>
2025-06-09 18:11:21 +0530

    html

--- rectangle.py
+++ rectangle.py
@@ -3,3 +3,8 @@
 # print (b*h)
 li=[1,2,3,4,5,6,7,8,9]
 li.insert(3,100)
+# b = int (input("enter the base"))
+# h = int (input("enter the height"))
+# print (b*h)
+li=[1,2,3,4,5,6,7,8,9]
+li.insert(3,100)
