254ef6d45285cc34cf9f16e33663b6a1d006728e
Pratima kushwa <pratimakushwaha479@gmail.com>
2025-06-10 22:37:09 +0530

    py

--- fg.py
+++ fg.py
@@ -8,3 +8,18 @@ if num%4==0 and num%400==0:
     print("leap year")
 else:
     print("not a leap year")
+num = int (input("enter the number"))
+for i in range(1,num):
+    if num%i==0:
+        print("square")
+    else:
+        print("not divisiable")  
+
+
+
+        num = int (input("enter the number"))
+for i in range(1,num):
+    if num%i==0:
+        print("square")
+    else:
+        print("not divisiable")  
