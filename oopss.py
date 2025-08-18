faa2b977f6c10a654e9a5d83d80d5689b02378d3
Pratima kushwa <pratimakushwaha479@gmail.com>
2025-08-18 18:31:12 +0530

    py

--- oopss.py
+++ oopss.py
@@ -27,27 +27,58 @@
 # s1=Student('karan',[22,33,44])
 # s1.get_avg()
        
-class amount:
-    def __init__(self,balance,account):
-        self.balance=balance
-        self.account=account
-    def debit(self,amount):
-        self. balance -=amount
-        print('rs',amount,"was debit")
-        print('total balance = ', self.get_balance())
-    def credit(self,amount):
-        self.balance +=amount
-        print('rs',amount,"was creadit")
-        print('total balance = ', self.get_balance())
+# class amount:
+#     def __init__(self,balance,account):
+#         self.balance=balance
+#         self.account=account
+#     def debit(self,amount):
+#         self. balance -=amount
+#         print('rs',amount,"was debit")
+#         print('total balance = ', self.get_balance())
+#     def credit(self,amount):
+#         self.balance +=amount
+#         print('rs',amount,"was creadit")
+#         print('total balance = ', self.get_balance())
 
-    def get_balance(self):
-        return self.balance
+#     def get_balance(self):
+#         return self.balance
 
 
     
         
 
-s1=amount(1000,1234)
-s1.debit(200)
-s1.credit(46)
-s1.get_balance()
+# s1=amount(1000,1234)
+# s1.debit(200)
+# s1.credit(46)
+# s1.get_balance()
+
+
+class Student:
+    school='SHSS'
+    def __init__(self ,name,city):
+        self.name=name
+        self.city=city
+    @classmethod
+    def update(cls,new):
+        print(id(cls))
+        cls.school=new
+    def show(self):
+        print(self.name,self.city,Student.school)
+print(id(Student))
+obj=Student('pratima','bhopal')
+obj.update('new-shss')
+obj.show()
+obj1=Student('pratima','bhopal')
+obj1.show()
+
+
+
+
+
+
+#     def add():
+#         print(self.name)
+#         print(self.city)
+# obj=Student("pratima","bhopal")
+# obj.add()
+        
