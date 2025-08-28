1e3998c9717b5cdda3ecec924f18cb2b0af1c3d4
Pratima kushwa <pratimakushwaha479@gmail.com>
2025-08-28 18:08:26 +0530

    pratima

--- pratice2.py
+++ pratice2.py
@@ -120,17 +120,107 @@
 #     i=i+1
 # print(fact)
 
-n=input("enter the string")
-vowel="aeiouAEIOU"
-even=0
-odd=0
-i=0
-while i < len(n):
-    char=n[i]
-    if char in vowel:
-        even=even+1
-    else:
-        odd=odd+1
-i=i+1
-print(even)
-print(odd)
+# n=input("enter the string")
+# vowel="aeiouAEIOU"
+# even=0
+# odd=0
+# i=0
+# while i < len(n):
+#     char=n[i]
+#     if char in vowel:
+#         even=even+1
+#     else:
+#         odd=odd+1
+# i=i+1
+# print(even)
+# print(odd)
+
+
+# string
+# s1="hello"
+# s2="ram"
+# print(s1*s2)
+
+
+# list
+# s1=[1,12,3,5,6]
+# s2=[4,5,6,8,0]
+# print(s1+s2)
+# print(s1-s2)
+# print(s1*0)
+# print(s1//s2)
+# print(s1>s2)
+# print(s1<=s2)
+# print(s1==s2)
+# print(s1 !=s2)
+# print(s1 and s2)
+# print(s1 or s2)
+# print(s1 in s2)
+# print(s1 not in s2)
+# print(s1 is s2)
+# print(s1 is not s2)
+# print(s1 & s2 )
+# print(s1 | s2)
+# print(s1 >> s2)
+# print(s1<< s2)
+
+
+data1={
+    "name":"gauri",
+    "age":"22"
+}
+data2={
+    "name":"pratima",
+    "age":21,
+}
+# # print(data1+data2)
+# # print(data1-data2)
+# # print(data1*data2)
+# # print(data1%data2)
+# # print(data1//data2)
+# print(data1*data2)
+# print(data1//data2)
+# print(data1>data2)
+# print(data1<=data2)
+# print(data1==data2) //
+# print(data1 !=data2)//
+# print(data1 and data2)// higher charcter
+# print(data1 or data2) less character
+# print(data1 in data2)
+# print(data1 not in data2)
+# print(data1 is not data2) //
+# print(data1 is data2) //
+# print(data1 & data2 )
+# print(data1 | data2)
+# print(data1 >> data2)
+# print(data1<< data2)
+
+
+# touple
+
+
+s1=(1,12,3,5,6)
+s2=(4,5,6,8,0)
+# print(s1+s2)//
+# print(s1-s2)
+# print(s1*0)//
+# print( s1 += s2)
+# print(s1-=s2)
+# print(s1*=s2)//
+# print(s1//=s2)
+# print(s1 %= s2)
+# print(s1>s2)//
+# print(s1<=s2)//
+# print(s1==s2)//
+# print(s1 !=s2)//
+# print(s1 and s2)//
+# print(s1 or s2)//
+# print(s1 in s2)//
+# print(s1 not in s2)//
+# print(s1 is s2)//
+# print(s1 is not s2)//
+# print(s1 & s2 )
+# print(s1 | s2)
+# print(s1 >> s2)
+# print(s1<< s2)
+
