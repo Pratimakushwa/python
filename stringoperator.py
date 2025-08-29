d000b6f3c87da23e90f02daa602104533400e4ae
Pratima kushwa <pratimakushwaha479@gmail.com>
2025-08-29 21:12:00 +0530

    pratima

---
+++ stringoperator.py
@@ -0,0 +1,88 @@
+# string
+# s1="hello"
+# s2="ram"
+# print(s1*s2)
+
+
+# list
+# s1=[1,12,3,5,6]
+# s2=[4,5,6,8,0]
+# print(s1+s2)
+# print(s1-s2)
+# print(s1*0)
+# print(s1//s2)
+# print(s1>s2)
+# print(s1<=s2)
+# print(s1==s2)
+# print(s1 !=s2)
+# print(s1 and s2)
+# print(s1 or s2)
+# print(s1 in s2)
+# print(s1 not in s2)
+# print(s1 is s2)
+# print(s1 is not s2)
+# print(s1 & s2 )
+# print(s1 | s2)
+# print(s1 >> s2)
+# print(s1<< s2)
+
+
+data1={
+    "name":"gauri",
+    "age":"22"
+}
+data2={
+    "name":"pratima",
+    "age":21,
+}
+# # print(data1+data2)
+# # print(data1-data2)
+# # print(data1*data2)
+# # print(data1%data2)
+# # print(data1//data2)
+# print(data1*data2)
+# print(data1//data2)
+# print(data1>data2)
+# print(data1<=data2)
+# print(data1==data2) //
+# print(data1 !=data2)//
+# print(data1 and data2)// higher charcter
+# print(data1 or data2) less character
+# print(data1 in data2)
+# print(data1 not in data2)
+# print(data1 is not data2) //
+# print(data1 is data2) //
+# print(data1 & data2 )
+# print(data1 | data2)
+# print(data1 >> data2)
+# print(data1<< data2)
+
+
+# touple
+
+
+s1=(1,12,3,5,6)
+s2=(4,5,6,8,0)
+# print(s1+s2)//
+# print(s1-s2)
+# print(s1*0)//
+# print( s1 += s2)
+# print(s1-=s2)
+# print(s1*=s2)//
+# print(s1//=s2)
+# print(s1 %= s2)
+# print(s1>s2)//
+# print(s1<=s2)//
+# print(s1==s2)//
+# print(s1 !=s2)//
+# print(s1 and s2)//
+# print(s1 or s2)//
+# print(s1 in s2)//
+# print(s1 not in s2)//
+# print(s1 is s2)//
+# print(s1 is not s2)//
+# print(s1 & s2 )
+# print(s1 | s2)
+# print(s1 >> s2)
+# print(s1<< s2)
+
